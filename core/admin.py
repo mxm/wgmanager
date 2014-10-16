293c48e21c5f687345426dfe2c89e59b730e647c
mxm <mxm@zedat.fu-berlin.de>
2014-10-16 22:24:37 +0200

    register core model with django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Shopping, Shop, Bill, Tag, Payer
+
+admin.site.register(Shopping)
+admin.site.register(Shop)
+admin.site.register(Bill)
+admin.site.register(Tag)
+admin.site.register(Payer)
